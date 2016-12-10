cf58a0d67c2a794b0c8c25209a86933e4500bcb5
thangtqvn <quocthang.2001@gmail.com>
2016-12-10 23:32:30 +0700

    Len de

--- extras/cors.php
+++ extras/cors.php
@@ -18,6 +18,7 @@ function function6(){}
 function function7(){}
 function function8(){}
 function function9(){}
+function function10(){}
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');

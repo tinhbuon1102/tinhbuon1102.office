4b017e4ca40ee33770ee3f29d8c14b5f57744735
nhatnam1102 <quocthang_2001@yahoo.com>
2016-12-10 23:38:52 +0700

    Merged

--- extras/cors.php
+++ extras/cors.php
@@ -17,6 +17,9 @@ function function5(){}
 function function6(){}
 function function7(){}
 function function8(){}
+function functtion11(){}
+function function9(){}
+function function10(){}
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');

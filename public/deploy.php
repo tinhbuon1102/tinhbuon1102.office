35da83286dc2835c0695971a0dd9c56201bf8470
thangtqvn <quocthang.2001@gmail.com>
2017-02-06 20:28:12 +0700

    Upload files

--- public/deploy.php
+++ public/deploy.php
@@ -1,6 +1,9 @@
 <?php
 
-$ret = exec('/home/coreworking/opt/bin/git add -u ../; /home/coreworking/opt/bin/git add ../; /home/coreworking/opt/bin/git commit -m "Upload files"; /home/coreworking/opt/bin/git push origin master;');
+$ret = exec('/home/coreworking/opt/bin/git reset --hard HEAD;', $out, $err);
+$ret = exec('/home/coreworking/opt/bin/git add -u ../;', $out, $err);
+$ret = exec('/home/coreworking/opt/bin/git commit -m "Upload files";', $out, $err);
+$ret = exec(' /home/coreworking/opt/bin/git push origin master;', $out, $err);
 if ($err) {
 	echo ('Error : <br />');
 	echo '<pre>'; 

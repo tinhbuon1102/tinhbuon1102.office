9f7dd3f130447ce9286ec191da48f350b424de4f
thangtqvn <quocthang.2001@gmail.com>
2017-02-06 14:28:39 +0700

    Upload files

--- public/livechat/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-header('Location: /livechat/php/app.php?admin');
--- public/livechat/sql/uninstall_mysql.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE %db_name%.mirrormx_customer_chat_user;
-
-DROP TABLE %db_name%.mirrormx_customer_chat_message;
-
-DROP TABLE %db_name%.mirrormx_customer_chat_data;

6e5d244b7333842ee17e5755f12eaa48a402b298
tinhbuon1102 <quocthang.2001@gmail.com>
2017-02-06 23:15:20 +0900

    Upload files

--- public/deploy.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$ret = shell_exec('/home/coreworking/opt/bin/git reset --soft HEAD 2>&1; /home/coreworking/opt/bin/git add -u ../. 2>&1; /home/coreworking/opt/bin/git add ../ 2>&1; /home/coreworking/opt/bin/git commit -m "Deploy files From Test Environment" 2>&1; /home/coreworking/opt/bin/git push origin master 2>&1;');
-echo '<pre>';
-print($ret);
-
-?>

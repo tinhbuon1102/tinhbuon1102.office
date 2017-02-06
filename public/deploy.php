eaffd97baae87b5b942287eee67caf23bcacce14
thangtqvn <quocthang.2001@gmail.com>
2017-02-06 20:51:14 +0700

    Upload files

--- public/deploy.php
+++ public/deploy.php
@@ -1,11 +1,14 @@
 <?php
 
+$ret = exec('/home/coreworking/opt/bin/git reset --soft HEAD;', $out, $err);
+$ret = exec('/home/coreworking/opt/bin/git add -u ../.;', $out, $err);
+$ret = exec('/home/coreworking/opt/bin/git add ../;', $out, $err);
+$ret = exec('/home/coreworking/opt/bin/git commit -m "Deploy files From Test Environment";', $out, $err);
 $ret = exec('/home/coreworking/opt/bin/git push origin master;', $out, $err);
 if ($err) {
 	echo ('Error : <br />');
 	echo '<pre>'; 
 	print($err);
-	print_r($out);die;
 }
 else {
 	echo '<pre>'; 

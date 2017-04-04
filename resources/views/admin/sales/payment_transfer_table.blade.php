05200495ec9851d2bd6bb9898d0170ca0be2f4ca
thangtqvn <quocthang.2001@gmail.com>
2017-04-05 00:47:44 +0700

    Upload files

--- resources/views/admin/sales/payment_transfer_table.blade.php
+++ resources/views/admin/sales/payment_transfer_table.blade.php
@@ -4,6 +4,15 @@
 }else {
 	$bankName = $rent->shareUser->bank->BankName;
 }
+
+$params = array();
+if (isset($_REQUEST['filter_time']) && $_REQUEST['filter_time'])
+	$params['filter_time'] = $_REQUEST['filter_time'];
+if (isset($_REQUEST['start_date']) && $_REQUEST['start_date'])
+{
+	$params['start_date'] = $_REQUEST['start_date'];
+	$params['end_date'] = $_REQUEST['end_date'];
+}
 ?>
 	<tr class="gradeX odd" role="row">
 		<td>#{{$rent->shareUser->id}}</td>
@@ -12,7 +21,7 @@
 		<td>{{priceConvert($rent->total_amount - $rent->total_charge_fee, true)}}</td>
 		<td>{{priceConvert($rent->total_charge_fee * 2, true)}}</td>
 		<td>
-			<a target="_blank" href="/MyAdmin/Sales?detail=1&id={{$rent->shareUser->id}}&tab=transfer_list">詳細</a>
+			<a target="_blank" href="/MyAdmin/Sales?detail=1&id={{$rent->shareUser->id}}&tab=transfer_list<?php echo !empty($params) ? '&' . http_build_query($params) : ''?>">詳細</a>
 		</td>
 	</tr>
 @endforeach

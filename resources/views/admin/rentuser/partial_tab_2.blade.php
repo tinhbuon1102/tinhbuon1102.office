88edd280a3e425f227e1d452da4a19fdfad0668b
thangtqvn <quocthang.2001@gmail.com>
2017-06-08 23:24:33 +0700

    Upload files

--- resources/views/admin/rentuser/partial_tab_2.blade.php
+++ resources/views/admin/rentuser/partial_tab_2.blade.php
@@ -142,7 +142,7 @@
 												
 												@if($rent->status==BOOKING_STATUS_REFUNDED)
 												<p class="refund-fee default-fee">
-													<b> <?php echo getRefundPrice($rent, true, true)?></b>
+													<b> <?php echo getRefundChargedPrice($rent, true, true)?></b>
 												</p>
 												@endif
 												
